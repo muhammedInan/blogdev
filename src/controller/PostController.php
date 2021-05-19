775566b1dee57c351b5427f4deec662a406e6e75
inan <muhammed-inan@outlook.com>
2021-05-20 01:29:18 +0200

    listPosts in controller

--- src/controller/PostController.php
+++ src/controller/PostController.php
@@ -2,6 +2,16 @@
 
 namespace App\Controller;
 
+use App\Model\Db\PostManager;
+
 class PostController extends Controller {
 
+    public function listPosts()
+    {
+        $postManager =  new PostManager();
+        $posts = $postManager->getPosts();
+        return $this->render('post/listPosts.html.twig', array(
+            'posts' => $posts,
+        ));
+    }
 }
